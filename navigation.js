4a4e061333fa16bcc646d25a0225a13403c17c4c
nabtron <developer@nabtron.com>
2021-03-05 23:12:43 +0500

    Update navigation.js

--- navigation.js
+++ navigation.js
@@ -1,1 +1,51 @@
 // https://www.reactnativeschool.com/master-react-navigation-v5/installing-dependencies
+  const screens_with_tabbar = [undefined, 'Chats','Activity','Settings']
+
+  return (
+    <Tab.Navigator
+      screenOptions={({ route, index }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          // console.log(route)
+          let iconName;
+
+          if (route.name === "Chats") {
+            iconName = "chatbubbles";
+            // color = focused ? '' : 'red';
+          } else if (route.name === "Activity") {
+            iconName = "analytics";
+          } else if (route.name === "Settings") {
+            iconName = "cog";
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarVisible: (screens_with_tabbar.includes(getFocusedRouteNameFromRoute(route))),
+      })}
+      
+      tabBarOptions={{
+        activeTintColor: "#2196f3",
+        inactiveTintColor: "gray",
+        // tabBarVisible: false,
+      }}
+    >
+      <Tab.Screen
+        name="Chats"
+        options={{ tabBarBadge: 3 }}
+        // component={BaseScreen}
+        children={(props) => <BaseScreen {...props} tab="chats" />}
+      />
+      <Tab.Screen
+        name="Activity"
+        children={(props) => <BaseScreen {...props} tab="activity" />}
+      />
+      <Tab.Screen
+        name="Settings"
+        children={(props) => (
+          <BaseScreen
+            {...props}
+            tab="settings"
+          />
+        )}
+      />
+    </Tab.Navigator>
